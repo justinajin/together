eb82f912e5329d0db9f626035b239af15e0a265a
MIROO1220 <mir0_0@icloud.com>
2023-06-28 12:28:51 +0900

    제발 잘 올라가라~

---
+++ src/main/java/com/together/Fighting.java
@@ -0,0 +1,24 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Fighting
+ */
+@WebServlet("/fighting")
+public class Fighting extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("🥳🥳🥳우리 모두 화이팅 해보자구!! 아자아자 화이팅!!🥳🥳🥳");
+	}
+
+}
