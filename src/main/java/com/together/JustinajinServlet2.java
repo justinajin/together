fbaa73d0b3f7c22949bb774704f5dca6922aba45
justinajin <justinajin@gmail.com>
2023-06-28 12:25:42 +0900

    JustinajinServlet2 추가!

---
+++ src/main/java/com/together/JustinajinServlet2.java
@@ -0,0 +1,25 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JustinajinServlet2
+ */
+@WebServlet("/justinajin2")
+public class JustinajinServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("미루야 안뇽뇽뇽");
+	}
+
+}
